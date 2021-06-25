4f26b11b64826623316ea887a0eaad3f24a4407e
Linux User <mario@KissLibre.localdomain>
2021-06-25 21:42:25 +0000

    Prettify.

--- lua/general/mappings.lua
+++ lua/general/mappings.lua
@@ -1,93 +1,104 @@
--- Leader key
-vim.api.nvim_set_keymap('n', '<Space>', '<nop>', { noremap = true, silent = true })
+local map = vim.api.nvim_set_keymap
 
-vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', 'kj', '<esc>', { noremap = true, silent = true })
+-- Easy ESC
+map('i', 'jk', '<esc>', { noremap = true, silent = true })
+map('i', 'kj', '<esc>', { noremap = true, silent = true })
+map('i', '<C-c>', '<esc>', { noremap = true, silent = true })
 
 -- Easy CAPS
-vim.api.nvim_set_keymap('i', '<c-u>', '<esc>viwUi', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-u>', 'viwU<esc>', { noremap = true })
+map('i', '<c-u>', '<esc>viwUi', { noremap = true })
+map('n', '<c-u>', 'viwU<esc>', { noremap = true })
 
 -- TAB in normal mode will move to next buffer
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<cr>', { noremap = true, silent = true })
+map('n', '<TAB>', ':bnext<cr>', { noremap = true, silent = true })
+map('n', '<S-TAB>', ':bprevious<cr>', { noremap = true, silent = true })
 
 -- TAB completion
 -- function _G.smart_tab()
 -- return vim.fn.pumvisible() == 1 and [[\\<C-n>]] or [[\\<Tab>]]
 -- end
--- vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.smart_tab()', { expr = true, noremap = true })
-vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? \"\\<C-n>\" : \"\\<TAB>\"', { expr = true, noremap = true, silent = true })
+-- map('i', '<Tab>', 'v:lua.smart_tab()', { expr = true, noremap = true })
+map('i', '<Tab>', 'pumvisible() ? \"\\<C-n>\" : \"\\<TAB>\"', { expr = true, noremap = true, silent = true })
 
 -- Better tabbing
-vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
-vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
+map('v', '<', '<gv', { noremap = true })
+map('v', '>', '>gv', { noremap = true })
 
--- My configuration
---------------------
+---------- My configuration ----------
 
 -- Window and buffer management
 --------------------
-vim.api.nvim_set_keymap('n', '<Leader>h', ':wincmd h<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>j', ':wincmd j<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>k', ':wincmd k<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>l', ':wincmd l<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-c>', '<esc>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>v', ':vsplit<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>d', ':bd!<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>w', ':w<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader><Leader>', '/<++><cr>ca<', { noremap = true })
+map('n', '<Leader>h', ':wincmd h<cr>', { noremap = true })
+map('n', '<Leader>j', ':wincmd j<cr>', { noremap = true })
+map('n', '<Leader>k', ':wincmd k<cr>', { noremap = true })
+map('n', '<Leader>l', ':wincmd l<cr>', { noremap = true })
+map('n', '<C-h>', ':wincmd h<cr>', { noremap = true })
+map('n', '<C-j>', ':wincmd j<cr>', { noremap = true })
+map('n', '<C-k>', ':wincmd k<cr>', { noremap = true })
+map('n', '<C-l>', ':wincmd l<cr>', { noremap = true })
+map('n', '<C-c>', '<esc>', { noremap = true })
+map('n', '<Leader>v', ':vsplit<cr>', { noremap = true })
+map('n', '<Leader>d', ':bd!<cr>', { noremap = true })
+map('n', '<Leader>w', ':w<cr>', { noremap = true })
+map('n', '<Leader><Leader>', '/<++><cr>ca<', { noremap = true })
+
+-- Terminal
+--------------------
+map('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
+map('t', 'jk', '<C-\\><C-n>', { noremap = true })
 
 -- Registers
 --------------------
-vim.api.nvim_set_keymap('n', 'dD', '"_dd', { noremap = true })
+map('n', 'dD', '"_dd', { noremap = true })
 
--- Movements
---------------------
+---------- Movements ----------
+
 -- Make arrows useful
-vim.api.nvim_set_keymap('n', '<down>', 'ddp', {})
-vim.api.nvim_set_keymap('n', '<right>', 'dwea<space><esc>pxb', {})
-vim.api.nvim_set_keymap('n', '<left>', 'dwbPa<space><esc>b', {})
-vim.api.nvim_set_keymap('n', '<up>', 'ddkP', {})
+map('n', '<down>', 'ddp', {})
+map('n', '<right>', 'dwea<space><esc>pxb', {})
+map('n', '<left>', 'dwbPa<space><esc>b', {})
+map('n', '<up>', 'ddkP', {})
+map('n', '<cr>', 'i<cr><esc>k', {})
+map('n', 'E', 'gE', {})
+map('n', 'L', 'g$', {})
+map('n', 'g{', 'f{a', {})
+map('n', 'g}', 'F}i', {})
+
 -- Disable arrows
-vim.api.nvim_set_keymap('i', '<down>', '<nop>', {})
-vim.api.nvim_set_keymap('i', '<right>', '<nop>', {})
-vim.api.nvim_set_keymap('i', '<left>', '<nop>', {})
-vim.api.nvim_set_keymap('i', '<up>', '<nop>', {})
-vim.api.nvim_set_keymap('v', '<down>', '<nop>', {})
-vim.api.nvim_set_keymap('v', '<right>', '<nop>', {})
-vim.api.nvim_set_keymap('v', '<left>', '<nop>', {})
-vim.api.nvim_set_keymap('v', '<up>', '<nop>', {})
-vim.api.nvim_set_keymap('n', '<cr>', 'i<cr><esc>k', {})
-vim.api.nvim_set_keymap('n', 'E', 'gE', {})
-vim.api.nvim_set_keymap('n', 'L', 'g$', {})
-vim.api.nvim_set_keymap('n', 'g{', 'f{a', {})
-vim.api.nvim_set_keymap('n', 'g}', 'F}i', {})
+map('i', '<down>', '<nop>', {})
+map('i', '<right>', '<nop>', {})
+map('i', '<left>', '<nop>', {})
+map('i', '<up>', '<nop>', {})
+map('v', '<down>', '<nop>', {})
+map('v', '<right>', '<nop>', {})
+map('v', '<left>', '<nop>', {})
+map('v', '<up>', '<nop>', {})
 
 -- Make some room
 --------------------
-vim.api.nvim_set_keymap('n', 'go', 'o<esc>k', {})
-vim.api.nvim_set_keymap('n', 'gO', 'O<esc>j', {})
+map('n', 'go', 'o<esc>k', {})
+map('n', 'gO', 'O<esc>j', {})
 
 -- Center the screen
 --------------------
-vim.api.nvim_set_keymap('n', 'G', 'Gzz', { noremap = true })
-vim.api.nvim_set_keymap('n', 'n', 'nzz', {})
-vim.api.nvim_set_keymap('n', 'N', 'Nzz', {})
---
+map('n', 'G', 'Gzz', { noremap = true })
+map('n', 'n', 'nzz', {})
+map('n', 'N', 'Nzz', {})
+
 -- Shortcuts
 --------------------
-vim.api.nvim_set_keymap('n', '<Leader>fed', ':e $MYVIMRC<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>feR', ':luafile $MYVIMRC<cr>', { noremap = true })
+map('n', '<Leader>fed', ':e $MYVIMRC<cr>', { noremap = true })
+map('n', '<Leader>feR', ':luafile $MYVIMRC<cr>', { noremap = true })
+
+-- Custom Functions
+map('n', '<Leader>T', ':ThemeSwap<cr>', { noremap = true })
+
+---------- Plugins ----------
 
 -- FZF
 --------------------
-vim.api.nvim_set_keymap('n', '<Leader>f', ':FZF<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>w', 'yiw:Rg <C-r>"<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>\'', ':Marks<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>g', ':GFiles<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>r', ':Rg<cr>', {noremap = true})
+map('n', '<Leader>f', ':FZF<cr>', {noremap = true})
+map('n', '<Leader>w', 'yiw:Rg <C-r>"<cr>', {noremap = true})
+map('n', '<Leader>\'', ':Marks<cr>', {noremap = true})
+map('n', '<Leader>g', ':GFiles<cr>', {noremap = true})
+map('n', '<Leader>r', ':Rg<cr>', {noremap = true})
--- lua/general/settings.lua
+++ lua/general/settings.lua
@@ -1,44 +1,52 @@
 vim.g.mapleader = ','
 
-vim.api.nvim_set_option('syntax', 'enable')
-vim.api.nvim_set_option('hidden', true)
-vim.api.nvim_set_option('wrap', false)                          -- Display long lines as just one line
-vim.api.nvim_set_option('encoding', 'utf-8')                    -- The encoding displayed
-vim.api.nvim_set_option('pumheight', 10)                        -- Makes popup menu smaller
-vim.api.nvim_set_option('fileencoding', 'utf-8')                -- The encoding written to file
-vim.api.nvim_set_option('ruler', true)              			      -- Show the cursor position all the time
-vim.api.nvim_set_option('mouse', 'a')                           -- Enable your mouse
-vim.api.nvim_set_option('splitbelow', true)                     -- Horizontal splits will automatically be below
-vim.api.nvim_set_option('splitright', true)                     -- Vertical splits will automatically be to the right
-vim.api.nvim_set_option('t_Co', '256')                          -- Support 256 colors
-vim.api.nvim_set_option('conceallevel', 0)                      -- So that I can see `` in markdown files
--- vim.api.nvim_set_option('tabstop', 2)                           -- Insert 2 spaces for a tab
-vim.api.nvim_set_option('shiftwidth', 4)                        -- Change the number of space characters inserted for indentation
--- vim.api.nvim_set_option('smarttab', true)                       -- Makes tabbing smarter will realize you have 2 vs 4
-vim.api.nvim_set_option('expandtab', true)                      -- Converts tabs to spaces
-vim.api.nvim_set_option('smartindent', true)                    -- Makes indenting smart
-vim.api.nvim_set_option('autoindent', true)                     -- Good auto indent
-vim.api.nvim_set_option('laststatus', 2)                                           -- Always display the status line
-vim.api.nvim_exec('set number relativenumber', true)
-vim.api.nvim_set_option('cul', true)                            -- Enable highlighting of the current line
-vim.api.nvim_set_option('background', 'dark')                   -- tell vim what the background color looks like
--- vim.api.nvim_set_option('showtabline', 2)                       -- Always show tabs
---set noshowmode                                                -- We don't need to see things like -- INSERT -- anymore
-vim.api.nvim_set_option('updatetime', 300)                      -- Faster completion
-vim.api.nvim_set_option('timeoutlen', 500)                      -- By default timeoutlen is 1000 ms
-vim.api.nvim_set_option('clipboard', 'unnamedplus')             -- Copy paste between vim and everything else
--- vim.api.nvim_set_option('colorcolumn', 80)                    -- 80 character lines
-vim.api.nvim_set_option('textwidth', 79)
+local set = vim.api.nvim_set_option
 
---vim.api.nvim_set_option('iskeyword+', '-                     	-- treat dash separated words as a word text object--
+set('syntax', 'enable')
+set('hidden', true)
+set('wrap', false)                          -- Display long lines as just one line
+set('encoding', 'utf-8')                    -- The encoding displayed
+set('pumheight', 10)                        -- Makes popup menu smaller
+set('fileencoding', 'utf-8')                -- The encoding written to file
+set('ruler', true)              			      -- Show the cursor position all the time
+set('mouse', 'a')                           -- Enable your mouse
+set('splitbelow', true)                     -- Horizontal splits will automatically be below
+set('splitright', true)                     -- Vertical splits will automatically be to the right
+set('t_Co', '256')                          -- Support 256 colors
+set('conceallevel', 0)                      -- So that I can see `` in markdown files
+-- set('tabstop', 2)                           -- Insert 2 spaces for a tab
+set('shiftwidth', 4)                        -- Change the number of space characters inserted for indentation
+-- set('smarttab', true)                       -- Makes tabbing smarter will realize you have 2 vs 4
+set('expandtab', true)                      -- Converts tabs to spaces
+set('smartindent', true)                    -- Makes indenting smart
+set('autoindent', true)                     -- Good auto indent
+set('laststatus', 2)                                           -- Always display the status line
+vim.api.nvim_exec('set number relativenumber', true)
+set('cul', true)                            -- Enable highlighting of the current line
+set('background', 'dark')                   -- tell vim what the background color looks like
+-- set('showtabline', 2)                       -- Always show tabs
+--set noshowmode                                                -- We don't need to see things like -- INSERT -- anymore
+set('updatetime', 300)                      -- Faster completion
+set('timeoutlen', 500)                      -- By default timeoutlen is 1000 ms
+set('clipboard', 'unnamedplus')             -- Copy paste between vim and everything else
+-- set('colorcolumn', 80)                    -- 80 character lines
+set('textwidth', 79)
+
+--set('iskeyword+', '-                     	-- treat dash separated words as a word text object--
 vim.api.nvim_exec('set formatoptions-=cro', true)              -- Stop newline continution of comments
 
 -- My configuration
 --------------------
-vim.api.nvim_set_option('ignorecase', true)
-vim.api.nvim_set_option('smartcase', true)
-vim.api.nvim_set_option('scrolloff', 8)
-vim.api.nvim_set_option('hlsearch', false)
+set('ignorecase', true)
+set('smartcase', true)
+set('scrolloff', 8)
+set('hlsearch', false)
+
+-- Netrw
+--------------------
+vim.g.netrw_banner=0
+vim.g.netrw_liststyle=3
+vim.g.netrw_winsize=30
 
 vim.cmd([[
 set colorcolumn=80
@@ -47,15 +55,20 @@ set tabstop=2 softtabstop=2 shiftwidth=2 expandtab
 "set list
 set smarttab
 "==========Autocommands==========
+  augroup misc
 autocmd InsertEnter * norm zz
 autocmd BufWritePre * %s/\s\+$//e " Remove trailing whitespace
 autocmd FileType * setlocal formatoptions-=cro
+  augroup END
 
 "==========C++==========
+  augroup cpp
 autocmd BufWritePre *.cpp,*.h,*.html,*.css :normal gg=G<CR>
 autocmd BufWritePre *.cpp %s/}$/};/e
+  augroup END
 
 "==========Latex==========
+  augroup latex
 autocmd FileType tex,plaintex nmap <silent><buffer> ,r :-1read ~/Documents/Personal/Latex/Templates/relazione.tex<CR>
 autocmd FileType tex,plaintex nmap <silent><buffer> ,a :-1read ~/Documents/Personal/Latex/Templates/template.tex<CR>
 autocmd FileType tex,plaintex imap <silent><buffer> ,e <ESC>:-1read ~/Documents/Personal/Latex/Templates/equation.tex<CR>
@@ -71,8 +84,10 @@ autocmd FileType tex,plaintex imap <buffer> oo \`{o}
 autocmd FileType tex,plaintex imap <buffer> ii \`{i}
 autocmd FileType tex,plaintex imap <buffer> uu \`{u}
 autocmd FileType tex,plaintex imap <buffer> EE \`{E}
+  augroup END
 
 "==========Groff=============
+  augroup groff
 autocmd FileType nroff nmap <buffer><Leader>s :w<CR> :!groff -e -ms % -T pdf > %:r.pdf<CR>
 autocmd FileType nroff nmap <buffer><Leader>p :!zathura %:r.pdf & disown<CR>
 autocmd FileType nroff imap <buffer> ee \[`e]
@@ -82,4 +97,5 @@ autocmd FileType nroff imap <buffer> oo \[`o]
 autocmd FileType nroff imap <buffer> uu \[`u]
 autocmd BufNew,Filetype xmath :-1read ~/Documents/Personal/groff_template.ms
 autocmd FileType xmath :set filetype=nroff
+  augroup END
 ]])
