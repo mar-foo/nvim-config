9ece360802b816b6fe84d6d646c51a4c0bbc6bc5
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2021-07-02 23:27:14 +0000

    Trim unused mappings

--- lua/general/mappings.lua
+++ lua/general/mappings.lua
@@ -3,26 +3,9 @@ local map = vim.api.nvim_set_keymap
 -- Easy ESC
 map('i', 'jk', '<esc>', { noremap = true, silent = true })
 map('i', 'kj', '<esc>', { noremap = true, silent = true })
-map('i', '<C-c>', '<esc>', { noremap = true, silent = true })
-
--- Easy CAPS
-map('i', '<c-u>', '<esc>viwUi', { noremap = true })
-map('n', '<c-u>', 'viwU<esc>', { noremap = true })
 
 -- TAB in normal mode will move to next buffer
 map('n', '<TAB>', ':bnext<cr>', { noremap = true, silent = true })
-map('n', '<S-TAB>', ':bprevious<cr>', { noremap = true, silent = true })
-
--- TAB completion
--- function _G.smart_tab()
--- return vim.fn.pumvisible() == 1 and [[\\<C-n>]] or [[\\<Tab>]]
--- end
--- map('i', '<Tab>', 'v:lua.smart_tab()', { expr = true, noremap = true })
-map('i', '<Tab>', 'pumvisible() ? \"\\<C-n>\" : \"\\<TAB>\"', { expr = true, noremap = true, silent = true })
-
--- Better tabbing
-map('v', '<', '<gv', { noremap = true })
-map('v', '>', '>gv', { noremap = true })
 
 ---------- My configuration ----------
 
@@ -32,15 +15,9 @@ map('n', '<Leader>h', ':wincmd h<cr>', { noremap = true })
 map('n', '<Leader>j', ':wincmd j<cr>', { noremap = true })
 map('n', '<Leader>k', ':wincmd k<cr>', { noremap = true })
 map('n', '<Leader>l', ':wincmd l<cr>', { noremap = true })
-map('n', '<C-h>', ':wincmd h<cr>', { noremap = true })
-map('n', '<C-j>', ':wincmd j<cr>', { noremap = true })
-map('n', '<C-k>', ':wincmd k<cr>', { noremap = true })
-map('n', '<C-l>', ':wincmd l<cr>', { noremap = true })
-map('n', '<C-c>', '<esc>', { noremap = true })
 map('n', '<Leader>v', ':vsplit<cr>:FZF<cr>', { noremap = true })
 map('n', '<Leader>d', ':bd!<cr>', { noremap = true })
-map('n', '<Leader>w', ':w<cr>', { noremap = true })
-map('n', '<Leader><Leader>', '/<++><cr>ca<', { noremap = true })
+map('n', '<Leader><space>', '/<++><cr>ca<', { noremap = true })
 
 -- Terminal
 --------------------
@@ -58,11 +35,6 @@ map('n', '<down>', 'ddp', {})
 map('n', '<right>', 'dwea<space><esc>pxb', {})
 map('n', '<left>', 'dwbPa<space><esc>b', {})
 map('n', '<up>', 'ddkP', {})
-map('n', '<cr>', 'i<cr><esc>k', {})
-map('n', 'E', 'gE', {})
-map('n', 'L', 'g$', {})
-map('n', 'g{', 'f{a', {})
-map('n', 'g}', 'F}i', {})
 
 -- Disable arrows
 map('i', '<down>', '<nop>', {})
@@ -85,19 +57,27 @@ map('n', 'G', 'Gzz', { noremap = true })
 map('n', 'n', 'nzz', {})
 map('n', 'N', 'Nzz', {})
 
+-- Useful remaps
+--------------------
+map('n', '<cr>', 'i<cr><esc>k', {})
+map('n', 'E', 'gE', {})
+map('n', 'L', 'g$', {})
+
 -- Shortcuts
 --------------------
-map('n', '<Leader>fed', ':e $MYVIMRC<cr>', { noremap = true })
-map('n', '<Leader>feR', ':luafile $MYVIMRC<cr>', { noremap = true })
+map('n', '<Leader>cc', ':e $MYVIMRC<cr>', { noremap = true })
+map('n', '<Leader>cr', ':luafile $MYVIMRC<cr>', { noremap = true })
 
 -- Custom Functions
+--------------------
 map('n', '<Leader>T', ':ThemeSwap<cr>', { noremap = true })
 
 ---------- Plugins ----------
 
 -- FZF
 --------------------
-map('n', '<Leader>f', ':FZF<cr>', {noremap = true})
+map('n', '<Leader>f', ':FZF ~<cr>', {noremap = true})
+map('n', '<Leader><Leader>', ':FZF<cr>', {noremap = true})
 -- map('n', '<Leader>w', 'yiw:Rg <C-r>"<cr>', {noremap = true})
 map('n', '<Leader>\'', ':Marks<cr>', {noremap = true})
 map('n', '<Leader>g', ':GFiles<cr>', {noremap = true})
