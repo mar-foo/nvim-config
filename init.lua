8d5813ab4b9f2412030f4cbda521b0a36167178e
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2022-03-17 22:26:53 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,17 +9,14 @@ require('plugins') -- Use Packer.nvim to manage plugins
 
 -- Theme
 require('theme.theme') -- Color theme
-require('theme.evil-lualine') -- Lualine
 
 -- Lsp
 require('my-lsp.treesitter-config') -- Treesitter parsing
 require('my-lsp.lsp-config') -- Default LSP config
-require('my-lsp.bashls-lsp') -- Bash LSP
 require('lspconfig').ccls.setup{} -- C/C++ LSP
-require('my-lsp.sumneko-lsp') -- Lua LSP
-require('my-lsp.texlab-lsp') -- Texlive/Latex LSP
 require('my-lsp.gopls-lsp') -- Golang LSP
-require('my-lsp.compe-config') -- Completion
+require('my-lsp.cmp-config')
+require('my-lsp.sumneko-lsp') -- Lua LSP
 
 -- Plugin configuration
 require('my-plugins.kommentary') -- Easy comments
