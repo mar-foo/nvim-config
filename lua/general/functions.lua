c4c0dc29727a77a57e55f13a0382756752a3e958
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2022-03-17 22:26:21 +0100

    Add Grep FZF function

--- lua/general/functions.lua
+++ lua/general/functions.lua
@@ -12,3 +12,4 @@ end
 
 -- Make functions executable in Ex mode
 vim.cmd('command! ThemeSwap lua ThemeSwap()')
+vim.cmd('command! -bang -nargs=* Grep call fzf#vim#grep("grep -n -R ".shellescape(<q-args>), 1, fzf#vim#with_preview(), <bang>0)')
