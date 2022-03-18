2a7ab48ba9ace7b4d1582d14ee9413da9e896605
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-03-18 22:27:10 +0100

    Make snippets work

--- lua/my-lsp/cmp-config.lua
+++ lua/my-lsp/cmp-config.lua
@@ -3,30 +3,41 @@ vim.o.completeopt = "menu,noselect,menuone"
 
 local cmp = require('cmp')
 cmp.setup({
-  mapping = {
-      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-  }, {
-    {name = 'buffer'}
-  })
+	snippet = {
+		expand = function(args)
+			vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	mapping = {
+		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
+		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+		['<C-y>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		},
+		['<C-e>'] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		['<CR>'] = cmp.mapping.confirm({select = true}),
+	},
+	sources = cmp.config.sources({
+		{name = 'nvim_lsp'},
+		{name = 'nvim_lua'},
+		{name = 'ultisnips'},
+		}, {
+			{name = 'buffer', keyword_length = 5},
+		{name = 'path'},
+	}),
 })
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('lspconfig')['ccls'].setup {
-  capabilites = capabilities
+	capabilites = capabilities
 }
 
 require('lspconfig')['gopls'].setup {
-  capabilites = capabilities
+	capabilites = capabilities
 }
