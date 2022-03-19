7f5b9fc022abf4cc85038d27cb9c2accc284d953
Mario Forzanini <mario.forzainin@studenti.unimi.it>
2022-03-19 14:39:04 +0100

    Reformat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,39 +6,36 @@ local fn = vim.fn
 local install_path = '/home/mario/.local/share/nvim/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+	fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+	execute 'packadd packer.nvim'
 end
 
 -- Plugins
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- LSP configuration
-  use 'neovim/nvim-lspconfig'
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+	-- LSP configuration
+	use 'neovim/nvim-lspconfig'
+	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 	--
-  -- Code completion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'SirVer/UltiSnips'
-  use 'quangnguyen30192/cmp-nvim-ultisnips'
-  use 'honza/vim-snippets'
+	-- Code completion
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-nvim-lua'
+	use 'SirVer/UltiSnips'
+	use 'quangnguyen30192/cmp-nvim-ultisnips'
+	use 'honza/vim-snippets'
 
-  -- Insert comments easily
-  use 'b3nj5m1n/kommentary'
-  -- Golang integration
-  use {'fatih/vim-go', ft = 'go'}
+	-- Insert comments easily
+	use 'b3nj5m1n/kommentary'
+	-- Golang integration
+	use {'fatih/vim-go', ft = 'go'}
 
-  -- Theme
-  use 'aditya-azad/candle-grey'
+	-- Theme
+	use 'aditya-azad/candle-grey'
 
-  -- FZF
-  use {'junegunn/fzf', run = function() vim.cmd[[fzf#install()]] end}
-  use 'junegunn/fzf.vim'
-	--
-  -- Git
-  use 'tpope/vim-fugitive'
+	-- FZF
+	use {'junegunn/fzf', run = function() vim.cmd[[fzf#install()]] end}
+	use 'junegunn/fzf.vim'
 end)
