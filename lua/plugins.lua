e275c7bee7c86cce246c2baaceabf7ca63661e63
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-07-04 20:50:00 +0200

    Nvim gdb

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
 	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 	--
 	use 'nvim-treesitter/nvim-treesitter-textobjects'
+	use {'sakhnik/nvim-gdb', run = './install.sh'}
 	-- Code completion
 	use 'hrsh7th/nvim-cmp'
 	use 'hrsh7th/cmp-nvim-lsp'
